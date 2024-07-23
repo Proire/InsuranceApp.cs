b264c33f255a6990c28f451f326955d725334023
Rohit Pagar <rohitpagar13@gmail.com>
2024-07-23 18:53:36 +0530

    [Rohit] Refactored

---
+++ InsuranceAppRLL.cs/CustomExceptions/PolicyException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceAppRLL.CustomExceptions
+{
+    public class PolicyException : ApplicationException
+    {
+        public PolicyException(string message)
+            : base(message)
+        {
+        }
+
+        public PolicyException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
