2589453e25673f64b3e25bef74ad32d7a98ee169
Rohit Pagar <rohitpagar13@gmail.com>
2024-07-24 17:32:00 +0530

    [Rohit] Added: Add Insurance Plan functionality added successfully

--- InsuranceAppRLL.cs/Repositories/Implementations/InsurancePlanRepository/InsurancePlanCommandRepository.cs
+++ InsuranceAppRLL.cs/Repositories/Implementations/InsurancePlanRepository/InsurancePlanCommandRepository.cs
@@ -1,13 +1,39 @@
-﻿using InsuranceAppRLL.Repositories.Interfaces.InsurancePlanRepository;
+﻿using InsuranceAppRLL.Entities;
+using InsuranceAppRLL.Repositories.Interfaces.InsurancePlanRepository;
+using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace InsuranceAppRLL.Repositories.Implementations.InsurancePlanRepository
 {
-    public class InsurancePlanCommandRepository: IInsurancePlanCommandRepository
+    public class InsurancePlanCommandRepository : IInsurancePlanCommandRepository
     {
+        private readonly InsuranceDbContext _context;
+
+        public InsurancePlanCommandRepository(InsuranceDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddPlan(InsurancePlan plan)
+        {
+            using(var transaction = _context.Database.BeginTransaction())
+            {
+                try
+                {
+                    await _context.InsurancePlans.AddAsync(plan);
+                    await _context.SaveChangesAsync();
+                    await transaction.CommitAsync();
+                }
+                catch(SqlException ex)
+                {
+                    transaction.Rollback();
+                    throw ex;
+                }
+            }
+        }
     }
 }
--- InsuranceAppRLL.cs/Repositories/Interfaces/InsurancePlanRepository/IInsurancePlanCommandRepository.cs
+++ InsuranceAppRLL.cs/Repositories/Interfaces/InsurancePlanRepository/IInsurancePlanCommandRepository.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using InsuranceAppRLL.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,6 @@ namespace InsuranceAppRLL.Repositories.Interfaces.InsurancePlanRepository
 {
     public interface IInsurancePlanCommandRepository
     {
+        public Task AddPlan(InsurancePlan plan); 
     }
 }
